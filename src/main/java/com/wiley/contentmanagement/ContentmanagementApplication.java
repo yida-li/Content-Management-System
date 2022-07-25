dd425002ac78d46cea9a06e709a15655e003ce2a
Jingyang Jiang <jingyang.jiang@mail.mcgill.ca>
2022-07-25 14:23:30 -0400

    spring intializer

---
+++ src/main/java/com/wiley/contentmanagement/ContentmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.wiley.contentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentmanagementApplication.class, args);
+	}
+
+}
