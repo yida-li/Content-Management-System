c729f408a1451cdb70eca437108a5ee6419220ac
Jingyang Jiang <jingyang.jiang@mail.mcgill.ca>
2022-08-01 17:47:24 -0400

    add tag fix

--- src/main/java/com/wiley/contentmanagement/controller/tagController.java
+++ src/main/java/com/wiley/contentmanagement/controller/tagController.java
@@ -2,6 +2,9 @@ package com.wiley.contentmanagement.controller;
 
 import com.wiley.contentmanagement.model.Tag;
 import com.wiley.contentmanagement.service.TagService;
+import java.util.Enumeration;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,51 +13,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.HashMap;
-
 @Controller
 public class tagController {
+
     @Autowired
     TagService tService;
 
     @GetMapping("/getAllTags")
-    public ResponseEntity getAllTags(Model model){
-        model.addAttribute("tags",tService.getAllTags());
+    public ResponseEntity getAllTags(Model model) {
+        model.addAttribute("tags", tService.getAllTags());
         return ResponseEntity.ok().body(tService.getAllTags());
     }
 
     @PostMapping("/addTag")
-    public String addTag(HttpServletRequest req, Model model){
-        Enumeration<String> names = req.getParameterNames();
-        HashMap<String,String[]> map = new HashMap<>();
-        while (names.hasMoreElements()) {
-            String name = names.nextElement();
-            String[] values = req.getParameterValues(name);
-            if(values!=null){
-                map.put(name, values);
-            }
-        }
+    public String addTag(String name, Model model) {
 
         Tag tag = new Tag();
-        tag.setName(map.get("name")[0]);
+        tag.setName(name);
         tService.addTag(tag);
-        model.addAttribute("tags",tService.getAllTags());
-        return "/tag.html";
+        model.addAttribute("tags", tService.getAllTags());
+        return "redirect:/tag";
     }
 
     @PostMapping("/editTag")
     public ResponseEntity editTag(HttpServletRequest req,
-                                  @RequestParam("id") int id,
-                                  Model model){
+            @RequestParam("id") int id,
+            Model model) {
 
         Enumeration<String> names = req.getParameterNames();
-        HashMap<String,String[]> map = new HashMap<>();
+        HashMap<String, String[]> map = new HashMap<>();
         while (names.hasMoreElements()) {
             String name = names.nextElement();
             String[] values = req.getParameterValues(name);
-            if(values!=null){
+            if (values != null) {
                 map.put(name, values);
             }
         }
@@ -67,9 +58,8 @@ public class tagController {
     }
 
     @GetMapping("/deleteTag")
-    public void deleteTag(@RequestParam("id") int id){
+    public void deleteTag(@RequestParam("id") int id) {
         tService.deleteTagById(id);
     }
-
 
 }
