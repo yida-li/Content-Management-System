d3afe56b405614e506d2627b7a41270c981b4c00
yida-li <gabrielyidali@example.com>
2022-08-02 16:56:58 -0400

    test

--- src/main/java/com/wiley/contentmanagement/ContentmanagementApplication.java
+++ src/main/java/com/wiley/contentmanagement/ContentmanagementApplication.java
@@ -2,6 +2,7 @@ package com.wiley.contentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = {"com.wiley.contentmanagement"})
 public class ContentmanagementApplication {
@@ -9,5 +10,9 @@ public class ContentmanagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContentmanagementApplication.class, args);
 	}
+
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ContentmanagementApplication.class);
 
 }
